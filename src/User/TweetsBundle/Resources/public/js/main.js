ff5d8ca7368e7fd9d8aa30f58b55950cf0f11dff
Ilija Matoski <ilijamt@gmail.com>
2014-05-28 00:17:23 +0200

    Now you can delete a tweet

--- src/User/TweetsBundle/Resources/public/js/main.js
+++ src/User/TweetsBundle/Resources/public/js/main.js
@@ -1,7 +1,40 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function() {
 
+    $("#tweet_container").on('click', '.btn-delete-tweet', function(evt) {
 
+        var data = $(this).data();
+
+        if (confirm("Are you sure you want to remove this tweet? ")) {
+            var settings = {
+                'contentType': 'application/json; charset=utf-8',
+                'url': "/api/tweets/" + data.id,
+                'type': "DELETE",
+                'data': JSON.stringify(data),
+                'async': true,
+                success: function(returnData, textStatus, jqXHR) {
+                    var $target = $("#" + data.target);
+                    if ($target.length) {
+                        $target.fadeOut(300, function() {
+                            $(this).remove();
+                        });
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    alert("Couldn't delete the tweet");
+                }};
+
+            jQuery.ajax(settings);
+
+        }
+
+    });
+
+    $("#tweet_container").on('click', '.btn-edit-tweet', function(evt) {
+        console.log("Edit tweet");
+    });
+
+    $("#new-tweet").on('click', function(evt) {
+        console.log("New tweet");
+    });
+
+});
